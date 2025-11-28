8442f0d428a1f8975a655a4d1dd55ec304ccc517
Acacore <mensart81@outllook.com>
2025-11-28 04:32:13 +0000

    Configure email with SMTP and env secrets

--- commerce/settings.py
+++ commerce/settings.py
@@ -268,7 +268,15 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = 'your-email@gmail.com'
+
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
